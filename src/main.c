1b3d6c2570740508c48140e1f731907425d1dc13
jakubt33 <jakub.trzyna@gmail.com>
2014-08-01 18:16:05 +0200

    delay add, inaqurate but sufficient

--- src/main.c
+++ src/main.c
@@ -6,9 +6,14 @@
 #include "init.h"
 
 
-void delay(volatile unsigned counter)
+void delay_ms(volatile unsigned counter)
 {
-  while(counter--);
+	counter*=1150;
+	while(counter--);
+}
+void delay_us(volatile unsigned counter)
+{
+	while(counter--);
 }
 
 int main(void)
@@ -16,9 +21,14 @@ int main(void)
 	init_LED();
 	GPIO_WriteBit(PORT_LED, LED_LEG1 | LED_LEG2 | LED_LEG3 | LED_LEG4 | LED_LEG5 | LED_LEG6, Bit_SET);
 
-	batt_1_5();
+	batt_5_5();
 	while (1)
 	{
+		delay_ms(1000);
+		LED_LEG1_OFF;
+
+		delay_ms(1000);
+		LED_LEG1_ON;
 
 	}
 }
