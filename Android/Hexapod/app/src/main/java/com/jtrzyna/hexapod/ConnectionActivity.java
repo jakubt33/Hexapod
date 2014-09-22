71384ce6d64b2053bccfbe47521600997dd40fba
jakubt33 <jakub.trzyna@gmail.com>
2014-09-22 22:23:26 +0200

    A: app crashes while connceting

--- Android/Hexapod/app/src/main/java/com/jtrzyna/hexapod/ConnectionActivity.java
+++ Android/Hexapod/app/src/main/java/com/jtrzyna/hexapod/ConnectionActivity.java
@@ -3,6 +3,7 @@ package com.jtrzyna.hexapod;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -19,7 +20,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class ConnectionActivity extends Activity implements AdapterView.OnItemClickListener {
@@ -29,6 +32,8 @@ public class ConnectionActivity extends Activity implements AdapterView.OnItemCl
     ArrayAdapter<String> mArrayAdapter;
     ListView gListView;
     Button bShowPairedDevices;
+    public static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,9 +87,6 @@ public class ConnectionActivity extends Activity implements AdapterView.OnItemCl
     public void onShowPairedDevicesClicked(View V){
         showPairedDevices();
     }
-    public void onRescanClicked(View V){
-
-    }
 
     protected void onActivityResult(int RequestCode, int ResultCode, Intent Data){
         super.onActivityResult(RequestCode,ResultCode,Data);
@@ -112,7 +114,63 @@ public class ConnectionActivity extends Activity implements AdapterView.OnItemCl
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
         Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
+
+        Object[] o = pairedDevices.toArray();
+        BluetoothDevice SelectedDevice = (BluetoothDevice) o[arg2];
+        ConnectThread Connect = new ConnectThread(SelectedDevice);
+       // Connect.start();
+
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        public final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) { }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+            manageConnectedSocket(mmSocket);
+        }
+
+        private void manageConnectedSocket(BluetoothSocket mmSocket) {
+        }
+
+        /** Will cancel an in-progress connection, and close the socket */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
     }
 }
