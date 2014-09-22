0c815a05b76b9d9b0673261415ba6dfcad60c70c
jakubt33 <jakub.trzyna@gmail.com>
2014-09-22 17:44:30 +0200

    Android: button: showPairedDevices

--- Android/Hexapod/app/src/main/java/com/jtrzyna/hexapod/ConnectionActivity.java
+++ Android/Hexapod/app/src/main/java/com/jtrzyna/hexapod/ConnectionActivity.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,12 +19,13 @@ import android.widget.Toast;
 import java.util.Set;
 
 
-public class ConnectionActivity extends Activity {
+public class ConnectionActivity extends Activity implements AdapterView.OnItemClickListener {
 
-    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    BluetoothAdapter mBluetoothAdapter;
+    Set<BluetoothDevice> pairedDevices;
     ArrayAdapter<String> mArrayAdapter;
     ListView gListView;
+    Button bShowPairedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +36,35 @@ public class ConnectionActivity extends Activity {
 
     private void init() {
 
+        bShowPairedDevices = (Button)findViewById(R.id.ShowPairedDevices);
         mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, 0);
         gListView = (ListView) findViewById(R.id.listView);
+        gListView.setOnItemClickListener(this);
         gListView.setAdapter(mArrayAdapter);
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBT();
+
+    }
+
+    private boolean checkBT(){
         if (mBluetoothAdapter == null) {
             //device doesn't support bluetooth activity
+            return false;
         }
         else {
             if (!mBluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 1);
             }
+            else return true;
+        }
+        return false;
+    }
 
+    private void showPairedDevices(){
+        if(checkBT()){
+            pairedDevices = mBluetoothAdapter.getBondedDevices();
             if (pairedDevices.size() > 0) {
                 // Loop through paired devices
                 for (BluetoothDevice device : pairedDevices) {
@@ -53,10 +73,19 @@ public class ConnectionActivity extends Activity {
                 }
             }
         }
+
     }
 
+    public void onShowPairedDevicesClicked(View V){
+        showPairedDevices();
+    }
 
-
+    protected void onActivityResult(int RequestCode, int ResultCode, Intent Data){
+        super.onActivityResult(RequestCode,ResultCode,Data);
+        if(ResultCode == RESULT_CANCELED){
+            Toast.makeText(getApplicationContext(), "Bluetooth must be enabled to continue", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -74,5 +103,10 @@ public class ConnectionActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
     }
 }
